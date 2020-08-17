089295cbe95debcd5ceb596b694b71419a50fe3d
BaxterDulieu <13939665@student.uts.edu.au>
2020-08-17 18:43:05 +1000

    100 percent complete

--- Assets/PrintAndHide.cs
+++ Assets/PrintAndHide.cs
@@ -4,19 +4,33 @@ using UnityEngine;
 
 public class PrintAndHide : MonoBehaviour
 {
+    public Renderer rend;
     int i = 0;
+    System.Random random = new System.Random();
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
+    
     // Update is called once per frame
     void Update()
     {
         
         ++i;
         Debug.Log(gameObject.name + ":" + i);
+
+        if ((i == 100) && gameObject.tag == "Red")
+        {
+            gameObject.SetActive(false);
+        }
+
+        if ((i == random.Next(200,250)) && gameObject.tag == "Blue")
+        {
+            rend.enabled = false;
+        }
     }
-    public Renderer rend;
+    
+    
 }
